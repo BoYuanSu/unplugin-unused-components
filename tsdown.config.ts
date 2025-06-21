9cf1ad00f9e5b275fdef7e33b87effda211da110
paul.su <mayday15221@gmail.com>
2025-06-21 17:44:21 +0800

    feat: update tsdown configuration to support multiple entry points

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,32 @@
-import { defineConfig } from 'tsdown'
+import { defineConfig } from 'tsdown';
 
-export default defineConfig({
-  entry: ['./src/*.ts'],
-  platform: 'neutral',
-  dts: {
-    isolatedDeclarations: true,
+export default defineConfig([
+  {
+    entry: ['./src/*.ts'],
+    platform: 'neutral',
+    dts: {
+      isolatedDeclarations: true,
+    },
+    outDir: 'dist/esm',
+    external: [
+      'node:path',
+      'node:fs',
+    ]
   },
-})
+  {
+    entry: ['./src/legacy/*.ts'],
+    format: ['cjs'],
+    platform: 'node',
+    target: 'node10',
+    outDir: 'dist/legacy',
+    dts: {
+      isolatedDeclarations: true,
+    },
+    external: [],
+    alias: {
+      'node:path': 'path',
+      'node:fs': 'fs',
+    },
+    shims: true
+  }
+]);
