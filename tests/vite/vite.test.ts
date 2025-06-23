bcae3b431785c621c6c981eeeef10a22db980a67
boyuansu <mayday15221@gmail.com>
2025-06-23 14:58:04 +0800

    test: add vite test cases

---
+++ tests/vite/vite.test.ts
@@ -0,0 +1,37 @@
+import { describe, it, vi, beforeEach, afterEach, expect } from 'vitest';
+import { Unused } from '../../src/index';
+import { build } from 'vite';
+import path from 'node:path';
+import vue from '@vitejs/plugin-vue';
+
+describe('vite', () => {
+  let consoleSpy: ReturnType<typeof vi.spyOn>;
+
+  beforeEach(() => {
+    // 監聽 console.log 以驗證 console 輸出
+    consoleSpy = vi.spyOn(console, 'log');
+  });
+
+  afterEach(() => {
+    // 還原 console.log
+    consoleSpy.mockRestore();
+  });
+
+  it('should run vite tests', async () => {
+    const { dirname } = import.meta;
+    await build({
+      build: {
+        lib: {
+          entry: path.resolve(dirname, '../fixtures/index.js'),
+          name: 'test',
+        },
+        rollupOptions: {
+          external: ['vue'],
+        },
+      },
+      plugins: [Unused.vite(), vue()],
+    });
+    // 驗證 console.log 是否有被呼叫
+    expect(consoleSpy).toBeCalledWith('Found 2 unused components:');
+  });
+});
