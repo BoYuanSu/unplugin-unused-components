400ca0efff8876ae54b59ec87cc3460659cd39e1
boyuansu <mayday15221@gmail.com>
2025-06-23 14:44:08 +0800

    test: add esbuild test cases

---
+++ tests/esbuild/esbuild.test.ts
@@ -0,0 +1,39 @@
+import { build } from 'esbuild';
+import { describe, it, vi, beforeEach, afterEach, expect } from 'vitest';
+import { Unused } from '../../src/index';
+import path from 'node:path';
+import vue from 'esbuild-plugin-vue';
+
+describe('esbuild', () => {
+  let consoleSpy: ReturnType<typeof vi.spyOn>;
+
+  beforeEach(() => {
+    // 監聽 console.log 以驗證 console 輸出
+    consoleSpy = vi.spyOn(console, 'log');
+  });
+
+  afterEach(() => {
+    // 還原 console.log
+    consoleSpy.mockRestore();
+  });
+
+  it('should run esbuild tests', async () => {
+    const { dirname } = import.meta;
+    await build({
+      entryPoints: [path.resolve(dirname, '../fixtures/index.js')],
+      plugins: [
+        Unused.esbuild(),
+        vue(),
+      ],
+      bundle: true,
+      define: {
+        __VUE_OPTIONS_API__: 'true',
+        __VUE_PROD_DEVTOOLS__: 'false',
+      },
+      outdir: path.resolve(dirname, '../fixtures/dist'),
+      external: ['vue'],
+    });
+    // 驗證 console.log 是否有被呼叫
+    expect(consoleSpy).toBeCalledWith('Found 2 unused components:');
+  });
+});
